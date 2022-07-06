acfd138671ab7b1b22e1f92594e23fd3c33df90b
olrlobt <317tmdgjs@naver.com>
2022-07-06 12:30:10 +0900

    asdasd

--- src/ascsiocpjsai.java
+++ src/ascsiocpjsai.java
@@ -1,4 +1,12 @@
 
 public class ascsiocpjsai {
-
+public static void main(String [] args) {
+		
+		String id = ""; 
+		String pw = "";
+		String name = "";
+		int birth;
+		String email = "";
+		
+}
 }
