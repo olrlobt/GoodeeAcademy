86cdf375b624a8a2b1ee29024fe97a8cb390765b
olrlobt <317tmdgjs@naver.com>
2022-07-07 09:15:08 +0900

    충돌테스트

--- src/com/iu/control/Control2.java
+++ src/com/iu/control/Control2.java
@@ -1,10 +1,24 @@
 package com.iu.control;
 
+import java.util.Scanner;
+
 public class Control2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		int num=0;
+		String wwww ="홀수";
+		
+		System.out.print("입력 : ");
+		Scanner sc = new Scanner(System.in);
+		num = sc.nextInt();
+		
+		if(num%2 == 0) {
+	        wwww = "짝수";
+	      }
+		
+		System.out.println(wwww + " 입니당 ");
+		
 	}
 
 }
