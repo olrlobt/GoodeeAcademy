de181465f08d546d735d1ed46f7ccec436a70141
EUNSH1 <tkdgus968@naver.com>
2022-07-13 13:46:15 +0900

    스튜던트뽑으래 뭔진모름

---
+++ src/com/iu/object1/ex1/StudentService.java
@@ -0,0 +1,15 @@
+package com.iu.object1.ex1;
+
+import java.util.Scanner;
+
+public class StudentService {
+
+	Scanner sc = new Scanner(System.in);
+	Student student = new Student();
+	//makeStudents 메서드 만들기
+	public void makerStudents() {
+		
+	}
+	//학생수를 입력받고 학생수만큼 정보를 입력
+	//학생들을 리턴
+}
