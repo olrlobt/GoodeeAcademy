65359fca55e5e53b88784020e651cd8f1f932468
olrlobt <317tmdgjs@naver.com>
2022-07-15 10:15:22 +0900

    hi

---
+++ src/com/iu/object3/Count.java
@@ -0,0 +1,20 @@
+package com.iu.object3;
+
+public class Count {
+
+	public void hap (int num1, int num2) {
+		System.out.println(num1+num2);
+		
+		
+		
+	}
+	
+	public void hap (int num1, double num2) {
+		System.out.println(num1+num2);
+		
+		
+		
+	}
+	
+	
+}
---
+++ src/com/iu/object3/object3Main2.java
@@ -0,0 +1,15 @@
+package com.iu.object3;
+
+public class object3Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Count count = new Count();
+		
+		count.hap(10,20);
+		
+		
+	}
+
+}
