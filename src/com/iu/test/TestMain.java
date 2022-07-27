3378f841bb6efe98cd300500082f52decaad4121
olrlobt <317tmdgjs@naver.com>
2022-07-27 17:11:57 +0900

    commit

--- src/com/iu/test/TestMain.java
+++ src/com/iu/test/TestMain.java
@@ -2,22 +2,28 @@ package com.iu.test;
 
 import java.sql.SQLException;
 
+import com.iu.countries.CountriesDAO;
+import com.iu.regions.RegionsDAO;
+import com.iu.regions.RegionsView;
 import com.iu.util.DBConnector;
 
 public class TestMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		DBConnector dbConnector = new DBConnector();
+//		DBConnector dbConnector = new DBConnector();
+		
+		CountriesDAO dao = new CountriesDAO();
+		RegionsDAO rdao = new RegionsDAO();
+		RegionsView view = new RegionsView();
 		
 		
-			try {
-				dbConnector.getConnection();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		
+		
+		view.view(rdao.getDetail(1));
 		}
+	
+	
 		
 	}
 
