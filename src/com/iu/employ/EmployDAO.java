67645257fc26c5517b539c46357e2267452b90cd
olrlobt <317tmdgjs@naver.com>
2022-08-01 09:16:43 +0900

    7/29pm

--- src/com/iu/employ/EmployDAO.java
+++ src/com/iu/employ/EmployDAO.java
@@ -1,10 +1,84 @@
 package com.iu.employ;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.iu.util.DBConnector;
+
 public class EmployDAO {
 
 	
+	public void getDetail(String country_name) throws Exception {
+		//1. DB 연결
+		Connection con = DBConnector.getConnection();
+		//2. SQL문 작성
+		String sql = "SELECT FIRST_NAME,\r\n"
+				+ "    SALARY , \r\n"
+				+ "    (SELECT FIRST_NAME FROM EMPLOYEES  WHERE EMPLOYEE_ID = 192)AS MANAGER_NAME, \r\n"
+				+ "    (SELECT SALARY FROM EMPLOYEES WHERE EMPLOYEE_ID = 192) AS MANAGER_SALARY\r\n"
+				+ "FROM EMPLOYEES  \r\n"
+				+ "WHERE EMPLOYEE_ID = (SELECT MANAGER_ID FROM EMPLOYEES  WHERE EMPLOYEE_ID = 192)\r\n"
+				+ ";\r\n"
+				+ "";
+		//3. 미리 전송
+		PreparedStatement st = con.prepareStatement(sql);
+		//4. ? 겂 새탕 =
+		
+		st.setString(1, country_name);
+		ResultSet rs = st.executeQuery();
+		
+		if(rs.next()) {
+			String rId= rs.getString("country_name");
+		
+			
+			System.out.println(rId);
+			
+			
+		}
+		
+
+		DBConnector.disConnect(rs, st, con);
+		
+	}
 	
 	
 	
 	
+	//1.Regions의 모든 데이터 가져오기
+		public void getList() throws Exception {
+			//1.DB 연결
+			Connection con = DBConnector.getConnection();
+			
+			
+			//2. query문 작성
+			String sql = "SELECT * FROM Countries";
+			
+			//3. Query문 미리 전송
+			
+			PreparedStatement st = con.prepareStatement(sql);
+			
+			//4. 
+			
+			//5. 최종 전송 후 결과 처리
+			ResultSet rs = st.executeQuery();
+			
+			
+			while(rs.next()) {
+				
+				int id = rs.getInt("Region_id");
+				String name = rs.getString("Country_name");
+				
+				System.out.println(id);
+				System.out.println(name);
+				
+				
+			}
+			
+			//6. 자원 해제
+			
+			DBConnector.disConnect(rs, st, con);
+			
+		}
+		
 }
