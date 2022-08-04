b18fa6d9d9070b34915f309106344588df1f59e8
olrlobt <317tmdgjs@naver.com>
2022-08-04 09:19:48 +0900

    08/03

--- src/main/java/com/iu/start/test/TestMain.java
+++ src/main/java/com/iu/start/test/TestMain.java
@@ -1,7 +1,10 @@
 package com.iu.start.test;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import com.iu.start.bankBook.BankBookDAO;
+import com.iu.start.bankBook.BankBookDTO;
 import com.iu.start.members.BankMembersDAO;
 import com.iu.start.members.BankMembersDTO;
 
@@ -21,19 +24,43 @@ public class TestMain {
 //		result = bankMembersDAO.setJoin(bankMembersDTO);
 		
 		
-//		System.out.println(result);
+////		System.out.println(result);
 		Scanner sc = new Scanner(System.in);
+//		
+//		BankMembersDTO bankMembersDTO = new BankMembersDTO();
+//		BankMembersDAO bankMembersDAO = new BankMembersDAO();
+//		
+//		
+//		String str = sc.next();
+//		bankMembersDAO.getSearchByID(str);
+//		long milliSecond = System.currentTimeMillis();
+		BankBookDAO bankBookDAO = new BankBookDAO();
+		BankBookDTO bankBookDTO = new BankBookDTO();
+		bankBookDTO.setBooknum( System.currentTimeMillis());
+		bankBookDTO.setBookname("dltmdgjs");
+		bankBookDTO.setBookrate(3.5);
+		bankBookDTO.setBooksale(false);
 		
-		BankMembersDTO bankMembersDTO = new BankMembersDTO();
-		BankMembersDAO bankMembersDAO = new BankMembersDAO();
-		
-		
-		String str = sc.next();
-		bankMembersDAO.getSearchByID(str);
+//		ArrayList<BankBookDTO> getlist = new ArrayList<BankBookDTO>();
 		
 		
 		
+		bankBookDAO.setBankBook(bankBookDTO);
+//		getlist = bankBookDAO.getList();
+//		
+//		for(BankBookDTO dto : getlist) {
+//			System.out.println("1");
+//			
+//		}
+		bankBookDAO.setChangeSale(bankBookDTO);
+		System.out.println("성공");
 		
+		BankBookDTO bankBookDTO1 = new BankBookDTO();
+		bankBookDTO1.setBooknum((long)1659507945370L);
+		
+		bankBookDTO1 =bankBookDAO.getDetail(bankBookDTO1);
+		
+		System.out.println(""+ bankBookDTO1.getBooknum() + bankBookDTO1.getBookname());
 		
 	}
 
